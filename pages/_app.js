d9d6efacff08343dd6d804b43392eb7761611a6c
Michael Griffiths <michael.griffiths@bglgroup.co.uk>
2021-02-25 16:27:42 +0000

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,33 @@ export default function App({ Component, pageProps }) {
               <title>Miss Ewe</title>
               <meta property="og:title" content="Home | Miss Ewe" key="title" />
               <meta name="robots" content="noindex" />
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+              />
+              <link rel="manifest" href="/site.webmanifest" />
+              <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#00cc00"
+              />
+              <meta name="apple-mobile-web-app-title" content="Miss Ewe" />
+              <meta name="application-name" content="Miss Ewe" />
+              <meta name="msapplication-TileColor" content="#00cc00" />
+              <meta name="theme-color" content="#00cc00" />
               <link rel="preconnect" href="https://fonts.gstatic.com" />
               <link
                 href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@400;600&family=Open+Sans:wght@300;400;700&display=swap"
