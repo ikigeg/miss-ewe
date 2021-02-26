9bf6fe3c1d85922e028a01e1919e676015742ade
Michael Griffiths <michael.griffiths@bglgroup.co.uk>
2021-02-26 16:18:46 +0000

    add link to logo

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,13 +4,15 @@ export default function Header() {
   return (
     <div>
       <h1>Miss Ewe</h1>
-      <img
-        src="img/missEwe.png"
-        style={{
-          width: 'auto',
-          maxHeight: '10rem',
-        }}
-      />
+      <a href="/" title="Home">
+        <img
+          src="img/missEwe.png"
+          style={{
+            width: 'auto',
+            maxHeight: '10rem',
+          }}
+        />
+      </a>
     </div>
   );
 }
