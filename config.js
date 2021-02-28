ee6ddfbe325ddb0131cbbaf6db6c6e707f8c8fe5
Michael Griffiths <michael.griffiths@bglgroup.co.uk>
2021-02-28 14:09:39 +0000

    multi config sucks

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-export const REDIRECT_URI = 'https://missewe.mrstyx.com/authed';
-export const CLIENT_ID = 'Iv1.95a7da62ada9504f';
+// export const REDIRECT_URI = 'https://missewe.mrstyx.com/authed';
+// export const CLIENT_ID = 'Iv1.95a7da62ada9504f';
+
+export const REDIRECT_URI = 'http://localhost:8080/authed';
+export const CLIENT_ID = 'Iv1.af449974fd3673a8';
