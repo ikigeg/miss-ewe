1e02213e46781d4f9669f0da18d60ea20d43fc29
Michael Griffiths <michael.griffiths@bglgroup.co.uk>
2021-02-28 18:47:45 +0000

    config

--- config.js
+++ config.js
@@ -1,5 +1,7 @@
-// export const REDIRECT_URI = 'https://missewe.mrstyx.com/authed';
-// export const CLIENT_ID = 'Iv1.95a7da62ada9504f';
+/* PROD */
+export const REDIRECT_URI = 'https://missewe.mrstyx.com/authed';
+export const CLIENT_ID = 'Iv1.95a7da62ada9504f';
 
-export const REDIRECT_URI = 'http://localhost:8080/authed';
-export const CLIENT_ID = 'Iv1.af449974fd3673a8';
+/* DEV */
+// export const REDIRECT_URI = 'http://localhost:8080/authed';
+// export const CLIENT_ID = 'Iv1.af449974fd3673a8';
